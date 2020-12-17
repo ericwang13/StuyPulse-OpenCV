2b61f2ee31a9abfcd7a16a2dc2c8f998205f1316
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2020-12-17 18:08:45 -0500

    Test thresholds yeah

--- Canny.py
+++ Canny.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plt
 
 cap = cv.VideoCapture(0)
 
@@ -14,12 +15,34 @@ while True:
         break
     rows, cols, channels = frame.shape
 
-    frame = cv.add(frame, cv.cvtColor(cv.Canny(frame,rows/5,cols/5),cv.COLOR_GRAY2BGR))
-    frame = cv.flip(frame, 1)
-    cv.imshow('Window Camera', frame)
+    m = np.median(frame)
 
-    k = cv.waitKey(1)
-    if cv.getWindowProperty('Window Camera', cv.WND_PROP_VISIBLE) < 1:
+    plt.subplot(1, 4, 1)
+    plt.imshow(frame)
+    plt.title("Original")
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.subplot(1, 4, 2)
+    plt.imshow(cv.Canny(frame, max(0,0.5*m), min(1.5 * m, 255)))
+    plt.title("0.5")
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.subplot(1, 4, 3)
+    plt.imshow(cv.Canny(frame, max(0,0.66*m), min(1.33 * m, 255)))
+    plt.title("0.66")
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.subplot(1, 4, 4)
+    plt.imshow(cv.Canny(frame, 255/3, 255))
+    plt.title("255 and 255/3")
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.show()
+    if cv.waitKey(1) == 'q' or cv.getWindowProperty('Window Camera', cv.WND_PROP_VISIBLE) < 1:
         break
 
 
